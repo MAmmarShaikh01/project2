d6564e0013f6b21076741aa3b367e7a2d434cbc3
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-09 21:29:30 +0500

    about

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        animation: {
+          fadeIn: 'fadeIn 1.5s ease-in-out',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: '0', transform: 'translateY(20px)' },
+            '100%': { opacity: '1', transform: 'translateY(0)' },
+          },
+        },
       },
     },
   },
   plugins: [],
+  
 } satisfies Config;
